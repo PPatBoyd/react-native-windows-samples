a188b4862b7e4efdde2e00665285ecf89e28fc64
Pat Boyd <ppatboyd@outlook.com>
2020-03-31 15:12:57 -0700

    delete samples

--- samples/NativeModuleSample/cppwinrt/windows/NativeModuleSample/pch.h
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#pragma once
-#include <unknwn.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
-
-#include <winrt/Microsoft.ReactNative.h>
